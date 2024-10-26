a2acbf2a0716fccd79765b3c0e26789dc1fc13ec
labtif unsur <labtif.unsur@gmail.com>
2024-10-26 18:14:22 +0700

    latihan pertemuan 4

---
+++ lib/latihanmodul/pertemuan4.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: BiggerText(text: 'Pertemuan 4'),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+  const Heading({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({super.key, required this.text});
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          widget.text,
+          style: TextStyle(fontSize: _textSize),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _textSize = 50.0;
+              });
+            },
+            child: Text('Perbesar'))
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
