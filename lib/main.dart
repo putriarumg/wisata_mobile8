e8b1181230b1b535afcac48c22d0922dc049be9f
Labtif Unsur <labtif.unsur@gmail.com>
2024-11-09 18:12:15 +0700

    Membuat Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
+import 'package:aslab_travel/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,7 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyApp(),
+      home: Splashscreen(),
     );
   }
 }
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Menunda selama 3 detik sebelum berpindah ke halaman berikutnya
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (context) => const Pertemuan5()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffF0F3F5),
+      body: Center(
+          child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Lottie.network(
+            'https://lottie.host/956e1e4f-8c98-4206-ae82-50dd50161d69/dtw01aXDDE.json',
+            width: 350,
+            height: 350,
+          ),
+          const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                height: 300,
+              ),
+              Text(
+                'ASLABTIF',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff1b1b1b),
+                ),
+              ),
+              Text(
+                'TRAVEL',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff1b1b1b),
+                ),
+              ),
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
